4da2e3883f7dabad232cfa6967de826c156d63a5
ashikul <alam.ashikul@gmail.com>
2017-01-21 12:36:42 -0500

    clear appId and password

--- app.js
+++ app.js
@@ -35,7 +35,5 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-
 bot.dialog('/', function (session) {
-    session.send("Hello World2");
-});
+    session.send("Hello World2")
