15d20a3fc969a234d3a4f69e2ecd56a28752b235
ashikul <alam.ashikul@gmail.com>
2017-01-21 12:45:00 -0500

    fix error

--- app.js
+++ app.js
@@ -35,5 +35,7 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
+
 bot.dialog('/', function (session) {
-    session.send("Hello World2")
+    session.send("Hello World2");
+});
